4fb34d2be3ea3971f3f480947fe13dfd5b5a2afc
Kristofer Andersen <kristoferatli@gmail.com>
2022-02-03 12:35:22 +0100

    Improving taskmanagerCLI

--- examples/TaskManagerCLI.cpp
+++ examples/TaskManagerCLI.cpp
@@ -33,65 +33,107 @@ void help() {
         << std::endl;
 }
 
+struct Action {
+    typedef taskman::TaskMan& M;
+    typedef std::vector<int>& Arg;
+
+    static void start(M m, Arg a) {
+        int id;
+        if(a.size() > 0) {
+            id = m.start(a[0]);
+        }
+        else {
+            // You can submit client defined tasks!
+            id = m.start(
+                std::shared_ptr<taskman::ITask>(
+                    new examples::ArbritraryTask()
+                )
+            );
+        }
+
+        std::cout << "Started " << id << std::endl;
+    }
+
+    static void pause(M m, Arg a) {
+        if(a.size() != 1) {
+            std::cout << "Task ID expected. Type \"help\" to see accepted command formats" << std::endl;
+            return;
+        }
+
+        bool success = m.pause(a[0]);
+        if(success) std::cout << "paused " << a[0] << std::endl;
+        else std::cout << "Failed to pause " << a[0] << std::endl;
+    }
+
+    static void stop(M m, Arg a) {
+        if(a.size() != 1) {
+            std::cout << "Task ID expected. Type \"help\" to see accepted command formats" << std::endl;
+            return;
+        }
+
+        bool success = m.stop(a[0]);
+        if(success) std::cout << "Stopped " << a[0] << std::endl;
+        else std::cout << "Failed to stop " << a[0] << std::endl;
+    }
+
+    static void status(M m, Arg a) {
+        std::vector<int> task_ids;
+
+        if(a.size() == 1) {
+            task_ids.push_back(a[0]);
+        }
+        else {
+            task_ids = m.get_task_ids();
+        }
+
+        for(int id : task_ids) {
+            auto t = m.get_task(id);
+            std::cout << *t << std::endl;
+        }
+    }
+};
+
 void command_loop() {
 
     taskman::TaskMan man;
 
     while(true) {
         std::string command;
-        int argument;
-        bool argument_given = false;
+        std::vector<int> args;
+        int arg;
 
         std::cin >> command;
 
         // There's more input
         if(std::cin.peek() != '\n') {
-            std::cin >> argument;
-            argument_given = true;
+            std::cin >> arg;
+            args.push_back(arg);
         }
 
         if(command == "help") {
             help();
         }
         else if(command == "start") {
-            int id;
-            if(argument_given) {
-                id = man.start(argument);
-            }
-            else {
-                // You can submit client defined tasks!
-                id = man.start(
-                    std::shared_ptr<taskman::ITask>(
-                        new examples::ArbritraryTask()
-                    )
-                );
-            }
-
-            std::cout << "Started " << id << std::endl;
+            Action::start(man, args);
         }
         else if(command == "pause") {
-            bool success = man.pause(argument);
-            if(success) std::cout << "paused " << argument << std::endl;
-            else std::cout << "Failed to pause " << argument << std::endl;
+            Action::pause(man, args);
         }
         else if(command == "stop") {
-            bool success = man.stop(argument);
-            if(success) std::cout << "Stopped " << argument << std::endl;
-            else std::cout << "Failed to stop " << argument << std::endl;
+            Action::stop(man, args);
         }
         else if(command == "status") {
-            std::cout << "Status:" << std::endl;
-            auto ids = man.get_task_ids();
-            
-            for(int id : ids) {
-                auto t = man.get_task(id);
-                std::cout << t << std::endl;
-            }
+            Action::status(man, args);
         }
         else if(command == "quit") {
             man.stop();
             break;
         }
+        else {
+            std::cout << "Command not recognized" << std::endl;
+        }
+
+        std::cout << std::endl;
     }
 }
 
